f9e51690a71a232195095daefb010c23af74c695
admin <paul_letailleur@hotmail.com>
2015-09-29 17:07:14 +0200

    ajout du symfony

---
+++ src/bundlePPEMission2/BootstrapBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace bundlePPEMission2\BootstrapBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('bundlePPEMission2BootstrapBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
