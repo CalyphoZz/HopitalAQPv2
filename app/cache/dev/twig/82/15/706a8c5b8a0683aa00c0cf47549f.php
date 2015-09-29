c53517518b2a13517b83434283291d6cba4f6036
CalyphoZz <quentin.debaudringhien@hotmail.fr>
2015-09-29 17:25:49 +0200

    routing controlleurs twig

---
+++ app/cache/dev/twig/82/15/706a8c5b8a0683aa00c0cf47549f.php
@@ -0,0 +1,49 @@
+<?php
+
+/* bundlePPEMission2BootstrapBundle:Default:accueil.html.twig */
+class __TwigTemplate_8215706a8c5b8a0683aa00c0cf47549f extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("bundlePPEMission2BootstrapBundle:Default:base.html.twig");
+
+        $this->blocks = array(
+            'corps' => array($this, 'block_corps'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "bundlePPEMission2BootstrapBundle:Default:base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 2
+    public function block_corps($context, array $blocks = array())
+    {
+        // line 3
+        echo "<h1> Accueil </h1>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "bundlePPEMission2BootstrapBundle:Default:accueil.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 3,  28 => 2,);
+    }
+}
--- src/bundlePPEMission2/BootstrapBundle/Controller/DefaultController.php
+++ src/bundlePPEMission2/BootstrapBundle/Controller/DefaultController.php
@@ -10,4 +10,33 @@ class DefaultController extends Controller
     {
         return $this->render('bundlePPEMission2BootstrapBundle:Default:index.html.twig', array('name' => $name));
     }
+    public function accueilAction()
+    {
+        return $this->render('bundlePPEMission2BootstrapBundle:Default:accueil.html.twig');
+    }
+
+    public function servicesAction()
+    {
+        return $this->render('bundlePPEMission2BootstrapBundle:Default:services.html.twig');
+    }
+    public function personnelAction()
+    {
+        return $this->render('bundlePPEMission2BootstrapBundle:Default:personnel.html.twig');
+    }
+    public function annuaireAction()
+    {
+        return $this->render('bundlePPEMission2BootstrapBundle:Default:annuaire.html.twig');
+    }
+    public function horairesAction()
+    {
+        return $this->render('bundlePPEMission2BootstrapBundle:Default:horaires.html.twig');
+    }
+    public function planAction()
+    {
+        return $this->render('bundlePPEMission2BootstrapBundle:Default:plan.html.twig');
+    }
+    public function infosAction()
+    {
+        return $this->render('bundlePPEMission2BootstrapBundle:Default:infos.html.twig');
+    }
 }
